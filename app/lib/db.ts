2ae65bffd727744aa4345406c928c3a6bd556ad0
Aditya Verma <adityavermabits@gmail.com>
2024-11-03 00:49:51 +0530

    fix prisma in db.ts

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,3 +1,16 @@
+// lib/db.ts
 import { PrismaClient } from "@prisma/client";
 
-export const prismaClient = new PrismaClient();
+// Avoid multiple instances of Prisma Client in development
+declare global {
+    // eslint-disable-next-line no-var
+    var prisma: PrismaClient | undefined;
+}
+
+export const prismaClient =
+    global.prisma ||
+    new PrismaClient({
+        log: ["query", "info", "warn", "error"],
+    });
+
+if (process.env.NODE_ENV !== "production") global.prisma = prismaClient;
